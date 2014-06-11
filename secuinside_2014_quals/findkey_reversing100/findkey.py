15032a43c747b7d20ee2e7c7d28295c46610dd96
mayfly74 <mayfly74@gmail.com>
2014-06-11 12:46:57 +0900

    push findkey writeup

---
+++ secuinside_2014_quals/findkey_reversing100/findkey.py
@@ -0,0 +1,127 @@
+#-*- coding:utf-8 -*-
+import numpy, sys, os, struct   
+
+# 0x804F180
+result = [0x00029344, 0x00029b98, 0x00020630, 0x0003a1f8,
+0x0001db6e, 0x00028a16, 0x00008e42, 0x000503c3,
+0x000547c2, 0x000557e3, 0x0005932c, 0x00056746,
+0x00058315, 0x0004c52d, 0x0005650a, 0x000497dd,
+0x0005841b, 0x0005d457, 0x00055db6, 0x0005a0ee,
+0x00049f50, 0x00056415, 0x0005601d, 0x0005552c,
+0x000524e7, 0x00037c93, 0x0003ba56, 0x0003c681,
+0x00038782, 0x0003c827, 0x00037e33, 0x0003a18d,
+0x000323be, 0x00039fc0, 0x0003db20, 0x00051e82,
+0x0005402b, 0x000555a5, 0x00047343, 0x0004fdcd,
+0x00052656, 0x0004cfb8, 0x00056153, 0x0005864c,
+0x000579c3, 0x000575f0, 0x00045213, 0x00051aa8,
+0x000558bd, 0x00056a64, 0x0004c639, 0x0004f711,
+0x0003f162, 0x0004d9f6, 0x0005188a, 0x00052808]
+
+# 0x804F0A0
+strtab = [ "No pain, No gain - gogil",
+"Fighting! - lokihardt",
+"Rock'n Roll~ - anncc",
+"WTF(Welcome To Facebook) - hellsonic",
+"Enjoy~ :) - hkpco",
+"I love meat - wooyaggo",
+" - v0n",
+"An algorithm must be seen to be believed. - Donald Knuth",
+"The purpose of computing is insight, not numbers. - Richard Hamming",
+"If you don't know anything about computers, just remember that they are machines that do exactly what you tell them but often surprise you in the result. - Richard Dawkins",
+"Computers are good at following instructions, but not at reading your mind. - Donald Knuth",
+"UNIX is user-friendly, it just chooses its friends. - Andreas Bogk",
+"The most important property of a program is whether it accomplishes the intention of its user. - C.A.R. Hoare",
+"The computing scientist’s main challenge is not to get confused by the complexities of his own making. -  Edsger W. Dijkstra",
+"One of my most productive days was throwing away 1000 lines of code. - Ken Thompson",
+"When in doubt, use brute force. - Ken Thompson",
+"The most effective debugging tool is still careful thought, coupled with judiciously placed print statements. - Brian W. Kernighan",
+"Controlling complexity is the essence of computer programming. - Brian W. Kernighan",
+"Mathematicians stand on each others' shoulders and computer scientists stand on each others' toes. - Richard Hamming",
+"Simplicity is prerequisite for reliability. - Edsger W. Dijkstra",
+"This ‘users are idiots, and are confused by functionality’ mentality of Gnome is a disease. If you think your users are idiots, only idiots will use it. - Linux",
+"Young man, in mathematics you don't understand things. You just get used to them. - John von Neumann",
+"If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.  -  John von Neumann",
+"The first principle is that you must not fool yourself - and you are the easiest person to fool. -Richard Feynman",
+"I don't know what's the matter with people: they don't learn by understanding; they learn by some other way - by rote or something. Their knowledge is so fragile! - Richard Feynman",
+"http://youtu.be/3O1_3zBUKM8 - youtube",
+"http://youtu.be/vc6vs-l5dkc - youtube",
+"http://youtu.be/v_3svzxSF4w - youtube",
+"http://youtu.be/AGlKJ8CHAg4 - youtube",
+"http://youtu.be/atz_aZA3rf0 - youtube",
+"http://youtu.be/KID8HAU2eBA - youtube",
+"http://youtu.be/_CL6n0FJZpk - youtube",
+"http://vimeo.com/70486448 - youtube",
+"http://youtu.be/HQPAN5dGbmc - youtube",
+"http://youtu.be/Sqz5dbs5zmo - youtube",
+"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind. - Bernard M. Baruch",
+"Be yourself; everyone else is already taken.  - Oscar Wilde",
+"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. - Albert Einstein",
+"So many books, so little time. - Frank Zappa",
+"Be the change that you wish to see in the world. - Mahatma Gandhi",
+"Live as if you were to die tomorrow. Learn as if you were to live forever - Mahatma Gandhi",
+"Don't walk behind me; I may not lead. Don't walk in front of me; I may not follow. Just walk beside me and be my friend. - Albert Camus",
+"If you tell the truth, you don't have to remember anything. - Mark Twain",
+"Whenever you find yourself on the side of the majority, it is time to pause and reflect. - Mark Twain",
+"A friend is someone who knows all about you and still loves you. - Elbert Hubbard",
+"The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself - Mark Caine",
+"I have not failed. I’ve just found 10,000 ways that won’t work. - Thomas A. Edison",
+"A successful man is one who can lay a firm foundation with the bricks others have thrown at him. - David Brinkley",
+"No one can make you feel inferior without your consent - Eleanor Roosevelt",
+"The successful warrior is the average man, with laser-like focus. - Bruce Lee",
+"If you don’t build your dream, someone else will hire you to help them build theirs - Dhirubhai Ambani",
+"There are but two powers in the world, the sword and the mind. In the long run the sword is always beaten by the mind. - Napoleon Bonaparte",
+"Stay hungry, Stay foolish. - Steve Jobs",
+"We're here to put a dent in the universe. - Steve Jobs",
+"Older people sit down and ask, 'What is it?' but the boy asks, 'What can I do with it? - Steve Jobs",
+"If you live each day as it was your last, someday you'll most certainly be right.  - Steve Jobs"]
+
+### Stage-1 ###
+print "[+] Stage-1 ... "
+n = 0x31
+matrix_a = [] 
+for i in range(len(strtab)) : 
+	#print strtab[i], result[i] 		
+	a = [struct.unpack('b', x)[0] for x in strtab[i].ljust(0x31, '\0')[:0x31]] 
+	matrix_a.append( a )
+key = numpy.matrix(matrix_a).I * numpy.matrix(result).T	
+
+key = ''.join([ chr(int(round(x.item(0)))) for x in key ])
+print "[>] 1st argument : ", key 
+
+### Stage-2 ###
+print "[+] Stage-2 ... "
+
+# 0x804F280
+table = [0, 1, 0, 1, 1, 1, 2, 4, 1, 3, 
+1, 6, 2, 4, 5, 2, 16, 17, 0, 16, 
+2, 14, 9, 1, 15, 9, 10, 14, 0, 15, 
+17, 27, 4, 17, 14, 10, 5, 7, 13, 21, 
+35, 9, 28, 25, 42, 23, 8, 45, 27]
+
+numbers = range(n)
+numbers.reverse()
+ans = [0]*n
+for i in range(n-1, -1, -1): 
+	num = numbers[table[i]]
+	ans[i]=num
+	numbers.remove(num)
+order = ans
+
+# check 
+#print '[>]', ''.join([ key[order.index(i)] for i in range(n)])
+
+ans = 0 
+for i in order[:0:-1] :
+	ans += i
+	ans *= n 
+ans += order[0] 
+order = str(ans)
+
+print "[>] 2nd argument : ", order 
+
+### run given file ### 
+print "[+] run program ... "
+target = "findkey_b2191f33d95fa6d2d0718efdbc78f8ae"
+os.execv(target, (target, key, order)) 
+
+
---
+++ secuinside_2014_quals/findkey_reversing100/readme.md
@@ -0,0 +1,153 @@
+Writeup for secuinside 2014 reversing 100 findkey
+-------------------------------------------------
+I couln't have enough time to solve this probem before the ctf ends, 
+so i can't check the flag is right. 
+
+### Description 
+*I can't remember :-(* <br>
+*something like 'find the key ...'*
+
+### analysis 
+```
+user@ubuntuvm:~/main/ctf/secuinside2014/findkey$ ./findkey_b2191f33d95fa6d2d0718efdbc78f8ae 
+need 2 arguments
+user@ubuntuvm:~/main/ctf/secuinside2014/findkey$ ./findkey_b2191f33d95fa6d2d0718efdbc78f8ae 1 2
+key 1 = 1
+0 is differnce
+Wrong password
+user@ubuntuvm:~/main/ctf/secuinside2014/findkey$ 
+```
+
+This program requires 2 argments. After some analysis, it turns out
+- 1st arguemnt : key strings 
+- 2nd arguemnt : chars order 
+
+if i find out right keyword chars and right order of chars, 
+i can get the flag. 
+
+##### 1-stage (find out key chars)
+function of checking first argument looks like this 
+
+```c
+  for ( i = 0; i <= 55; ++i )
+  {
+    v7 = 0LL;
+    for ( j = 0; ; ++j )
+    {
+      v4 = strlen((&str_table)[4 * i]);
+      v2 = which_small(&a2, &v4);
+      if ( *(_DWORD *)v2 <= j )
+        break;
+      v7 += (&str_table)[4 * i][j] * (signed int)*(_BYTE *)(j + a1);
+    }
+    if ( v7 != result_table[i] )
+    {
+      printf("%d is differnce\n", i);
+      return 0;
+    }
+  }
+```
+
+Each char of input strings converted to integer and multiplied each char of one of 55 strings(0x804F0A0). 
+And the sum of them should equal to value of result_table (0x804F180)
+so i can get multivariable linear equation. 
+For example, for the first string "No pain, No gain - gogil", i can get this equation. 
+
+```
+x0*78 + x1*111 + x2*32 + x3*112 + x4*97 + x5*105 + x6*110 + x7*44 + x8*32 + 
+x9*78 + x10*111 + x11*32 + x12*103 + x13*97 + x14*105 + x15*110 + x16*32 + 
+x17*45 + x18*32 + x19*103 + x20*111 + x21*103 + x22*105 + x23*108 = 0x00029344 // "No pain, No gain - gogil"
+```
+After solving this eqaution, i got this key char list which is the first argument of the program.
+
+```
+user@ubuntuvm:~/main/ctf/secuinside2014/findkey$ ./findkey_b2191f33d95fa6d2d0718efdbc78f8ae '3 lroea5 r  tfmh0wl1y15on 3y! 4n 50r,30wv3r !4kwi' 1
+key 1 = 3 lroea5 r  tfmh0wl1y15on 3y! 4n 50r,30wv3r !4kwi
+Wrong password
+```
+
+###### 2-stage (find out order)
+Next stage is finding out right order to reorder first argument string. 
+Second argument is converted to 49 integers like this.  
+
+```python 
+n = 49 # 0x31
+user_input = 'what?'
+user_input = [int(user_input[i:i+4]) for i in range(0, len(user_input), 4)]
+for index in range(n) :
+        v = 0
+        for i in range(len(user_input)) :
+                v = (10000 * v + user_input[i]) %n
+        integer_list.append(v) 
+        print v, 
+        # expected : 8, 4, 45, 9, 12, 26, 15, 10, 35, 17, 39, 14, 36, 32, 29, 37, 1, 0, 47, 5, 42, 11, 27, 46, 13, 31, 30, 19, 48, 23, 18, 6, 44, 22, 28, 34, 43, 41, 33, 25, 7, 38, 16, 20, 3, 24, 40, 2, 21
+
+        v = 0
+        for i in range(len(user_input)) :
+                v = 10000 * (v%n) + user_input[i]
+                user_input[i] = v/n
+```
+
+and checked each value like this.
+
+```c
+  for ( i = 0; i < n_copy; ++i )                
+  {
+    count = 0;
+    for ( j = 0; j < i; ++j )
+    {
+      v3 = *(_DWORD *)getvalue_by_index(&struct1, j);
+      if ( v3 > *(_DWORD *)getvalue_by_index(&struct1, i) )
+        ++count;
+    }
+    if ( tbl_countcheck[i] != count ) // tbl_countcheck = 0x804F280
+    {
+      v4 = 0;
+      goto FREE_RETURN;
+    }
+  }
+```  
+
+The right order is easily found by this code 
+```python
+# 0x804F280
+table = [0, 1, 0, 1, 1, 1, 2, 4, 1, 3, 
+1, 6, 2, 4, 5, 2, 16, 17, 0, 16, 
+2, 14, 9, 1, 15, 9, 10, 14, 0, 15, 
+17, 27, 4, 17, 14, 10, 5, 7, 13, 21, 
+35, 9, 28, 25, 42, 23, 8, 45, 27]
+
+numbers = range(n)
+numbers.reverse()
+ans = [0]*n
+for i in range(n-1, -1, -1): 
+	num = numbers[table[i]]
+	ans[i]=num
+	numbers.remove(num)
+order = ans
+```
+and i got this 
+
+```
+[8, 4, 45, 9, 12, 26, 15, 10, 35, 17, 39, 14, 36, 32, 29, 37, 1, 0, 47, 5, 42, 11, 27, 46, 13, 31, 30, 19, 48, 23, 18, 6, 44, 22, 28, 34, 43, 41, 33, 25, 7, 38, 16, 20, 3, 24, 40, 2, 21]
+```
+
+And now i got 2nd argument, 
+and finally i got the flag. :-) 
+
+```
+root@ubuntuvm:/home/user/main/ctf/secuinside2014/findkey# python findkey.py
+[+] Stage-1 ... 
+[>] 1st argument :  3 lroea5 r  tfmh0wl1y15on 3y! 4n 50r,30wv3r !4kwi
+[+] Stage-2 ... 
+[>] 2nd argument :  28367585747398446017812492718893415428463369378432457345198085366128794480569061784
+[+] run program ... 
+key 1 = 3 lroea5 r  tfmh0wl1y15on 3y! 4n 50r,30wv3r !4kwi
+The flag is : 'w0w! 1nv3r51on arr4y i5 4we50m3 f0r th3 k3y, lol!'
+root@ubuntuvm:/home/user/main/ctf/secuinside2014/findkey# 
+```
+
+source code : []
+
+** Sorry for my poor English. ** 
+
